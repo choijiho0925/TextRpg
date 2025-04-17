2501c33b1314d5cd66bc738411054cb7c9821a6f
choijiho0925 <jiho.0925.choi@gmail.com>
2025-04-18 04:54:57 +0900

    feat : 상점 재구매불가
    재구매불가

--- Store.cs
+++ Store.cs
@@ -8,15 +8,12 @@ namespace TextRPG
 {
     class Store
     {
-        public static List<Item> Items = new List<Item>()
+        List<Item> Items;
+        
+        public Store()
         {
-            new Item(1, "수련자 갑옷", "수련에 도움을 주는 갑옷입니다.", OptionType.Defense, 1000, 5, false),
-            new Item(2, "무쇠갑옷" , "무쇠로 만들어져 튼튼한 갑옷입니다.",OptionType.Defense, 2000, 9, false),
-            new Item(3, "스파르타의 갑옷", "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", OptionType.Defense, 3500, 15, false),
-            new Item(4, "낡은 검", "쉽게 볼 수 있는 낡은 검 입니다.", OptionType.Attack, 600, 2, false),
-            new Item(5, "청동 도끼", "어디선가 사용됐던거 같은 도끼입니다.", OptionType.Attack, 1500, 5, false),
-            new Item(6, "스파르타의 창", "스파르타의 전사들이 사용했다는 전설의 창입니다.", OptionType.Attack, 2500, 7, false)
-        };
+            Items = GameManager.Instance.Items;
+        }
 
         public void MainStore()
         {
@@ -64,7 +61,7 @@ namespace TextRPG
             Console.WriteLine("[아이템 목록]");
             foreach (Item item in Items)
             {
-                Console.WriteLine($"- {item.Id} | {item.Name} | {Item.StringOption(item.Type)}+{item.OptionValue} | {item.Description} | {item.Gold}G");
+                Console.WriteLine($"- {item.Id} | {item.Name} | {Item.StringOption(item.Type)}+{item.OptionValue} | {item.Description} | {(item.isBuy ? "구매완료" : $"{item.Gold}G")}");
             }
             Console.WriteLine("\n0.나가기");
             Console.WriteLine();
